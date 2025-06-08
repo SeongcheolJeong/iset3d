b48dae671ed9213f0f02e2d90ea0a16b7ae578df
SeongcheolJeong <mrsc129@gmail.com>
2025-06-08 12:45:19 +0900

    Add alias handling and helpers in Recipe

--- python/tests/test_recipe.py
+++ python/tests/test_recipe.py
@@ -20,3 +20,24 @@ def test_get_set():
     assert r.get('input file') == 'scene.pbrt'
     r.set('camera', {'type': 'pinhole'})
     assert r.get('camera')['type'] == 'pinhole'
+
+
+def test_synonyms():
+    r = Recipe()
+    r.set('inputfile', 'scene.pbrt')
+    assert r.get('input file') == 'scene.pbrt'
+    r.set('outputfile', '/tmp/out.pbrt')
+    assert r.get('output dir') == '/tmp'
+
+
+def test_convenience_methods():
+    r = Recipe()
+    r.add_light('main', {'type': 'point'})
+    assert r.get_light('main')['type'] == 'point'
+    r.remove_light('main')
+    assert r.get_light('main') is None
+
+    r.add_material('matte', {'kd': [1, 1, 1]})
+    assert r.get_material('matte')['kd'] == [1, 1, 1]
+    r.remove_material('matte')
+    assert r.get_material('matte') is None
